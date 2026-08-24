915ddb59f1994d28fa1b714426ecb344b1efd887
sanguine6660 <sanguine6660@gmail.com>
2026-08-24 14:31:15 +0200

    refactor(icons): proper lazy-loaded icon chunks generation

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,20 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     plugins: [preact(), tailwindcss()],
     base: '/yakuake-skin-generator/',
+    build: {
+        chunkSizeWarningLimit: 8096,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules/react-icons/')) {
+                        const match = id.match(/node_modules\/react-icons\/([a-z0-9]+)\//)
+                        if (match && match[1]) {
+                            return `icon-${match[1]}`
+                        }
+                        return 'vendor-icons'
+                    }
+                },
+            },
+        },
+    },
 })
